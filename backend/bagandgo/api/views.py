da0c5c67e392efb8ab100c2b0ee2f1cb018611d5
itu git <erk21@itu.edu.tr>
2024-10-20 02:08:34 +0300

    added login_view

--- backend/bagandgo/api/views.py
+++ backend/bagandgo/api/views.py
@@ -1,4 +1,24 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import permissions
+from django.contrib.auth import authenticate, login
+from rest_framework.permissions import AllowAny
+
+@api_view(['POST'])
+@permission_classes([AllowAny])  # Allows any user to access this endpoint
+def login_view(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    
+    if username is None or password is None:
+        return Response({'error': 'Please provide both username and password.'},
+                        status=status.HTTP_400_BAD_REQUEST)
+    
+    user = authenticate(username=username, password=password)
+    
+    if user is not None:
+        login(request, user)
+        return Response({'message': 'Login successful.'}, status=status.HTTP_200_OK)
+    else:
+        return Response({'error': 'Invalid credentials.'}, status=status.HTTP_401_UNAUTHORIZED)
