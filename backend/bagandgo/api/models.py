b8fa70ab652d3c49593f5669acbd2a64c545991a
Ataberk Çiftlikli <ataberkciftlikli@hotmail.com>
2024-10-20 17:44:35 +0300

    user model and serializers

--- backend/bagandgo/api/models.py
+++ backend/bagandgo/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from rest_framework.authtoken.models import Token
+
 
 # Create your models here.
+
+class AuthToken(models.Model):
+    token = models.CharField(max_length=255, unique=True)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.token} - {self.user.username}"
+
+class UserProfile(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    balance = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.user.username
