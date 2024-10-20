4f426e7661489d6aa3c95810d22ee5ea1df69daa
BirsuYilmaz <yilmazc20@itu.edu.tr>
2024-10-20 20:48:18 +0300

    Add the Login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 // App.tsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './components/Register/Register'; // Import the Register component
+import Login from './components/Login/Login'; // Import the Login component
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
