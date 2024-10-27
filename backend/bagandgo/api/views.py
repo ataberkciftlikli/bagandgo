86ae31445aa732d7ced3ea51733f97c65340e604
itu git <erk21@itu.edu.tr>
2024-10-28 00:28:54 +0300

    Added import serializers

--- backend/bagandgo/api/views.py
+++ backend/bagandgo/api/views.py
@@ -12,6 +12,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny
+from rest_framework import serializers
 
 from bs4 import BeautifulSoup
 import requests
