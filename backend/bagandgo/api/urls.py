ed1a4b03a21eee6e7d5119abf369bfd2cb907011
İTU Github Merthan <durdag21@itu.edu.tr>
2024-10-28 00:18:52 +0300

    Added URLs.

--- backend/bagandgo/api/urls.py
+++ backend/bagandgo/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import register_view, login_view, ProductCategoryViewSet, ProductViewSet
+from .views import register_view, login_view, ProductCategoryViewSet, ProductViewSet, add_to_cart, view_cart, checkout
 
 router = routers.DefaultRouter()
 
@@ -12,4 +12,7 @@ urlpatterns = [
     path('product/', include(router.urls)),
     path('register/', register_view, name='register-api'),
     path('login/', login_view, name = 'login-api'),
+    path('cart/add/', add_to_cart, name='add_to_cart'),
+    path('cart/view/', view_cart, name='view_cart'),
+    path('cart/checkout/', checkout, name='checkout'),
     ]
