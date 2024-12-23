7d966e5152141678b5d53ee8d5ea53876c856874
Orkan Isik <isikb21@itu.edu.tr>
2024-12-23 15:16:55 +0000

    Save local changes before merging with remote

--- backend/bagandgo/bagandgo/settings.py
+++ backend/bagandgo/bagandgo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=bimh&j+*&r#g^5c(=#g$c59x92(k-ue74hzdb8m6r4z)=u@9n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = [20.199.80.88]
+ALLOWED_HOSTS = ['20.199.80.252', 'localhost','192.168.50.93', 'localhost:5173']
 
 
 # Application definition
@@ -39,10 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'api'
+    'api',
+    'corsheaders', #yeni
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', #yeni
+    'django.middleware.common.CommonMiddleware', #yeni
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,9 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+#STATICFILES_DIRS = [BASE_DIR / '../../frontend']  #emin degilim part2
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True #yeni
