76a6b7356658aeefa1f23601900d8f230a464ff6
BirsuYilmaz <92296597+itu-itis21-yilmazc20@users.noreply.github.com>
2024-12-24 17:05:19 +0300

    Change of Category List

--- frontend/src/components/Category/CategoriesData.ts
+++ frontend/src/components/Category/CategoriesData.ts
@@ -2,16 +2,13 @@
 import { Category } from './types';
 
 export const categories: Category[] = [
-  { id: '1', name: 'Vegetables'},
-  { id: '2', name: 'Fruits'},
+  { id: '1', name: 'Vegetables and Fruits'},
+  { id: '2', name: 'Basic Foods'},
   { id: '3', name: 'Meat Products'},
   { id: '4', name: 'Dairy'},
   { id: '5', name: 'Snacks'},
   { id: '6', name: 'Bakery and Bread'},
   { id: '7', name: 'Beverages'},
-  { id: '8', name: 'Pantry'},
-  { id: '9', name: 'Frozen Food'},
-  { id: '10', name: 'Cleaning Products'},
-  { id: '11', name: 'Self Care Products'}
+  { id: '8', name: 'Self Care Products'}
   //{ id: '2', name: 'Electronics', subcategories: [{ id: '2-1', name: 'Mobile Phones' }] },
 ];
