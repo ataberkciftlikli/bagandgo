ce6fbff8d0718eab746f1700e9674dd7cd0971d5
Ataberk Çiftlikli <ataberkciftlikli@hotmail.com>
2025-04-12 15:19:35 +0300

    revert

--- backend/bagandgo/bagandgo/settings.py
+++ backend/bagandgo/bagandgo/settings.py
@@ -38,10 +38,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+<<<<<<< Updated upstream
     'api'
+=======
+    'rest_framework.authtoken',
+    'api',
+    'corsheaders', #yeni
+>>>>>>> Stashed changes
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', #yeni
+    'django.middleware.common.CommonMiddleware', #yeni
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +131,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True #yeni
