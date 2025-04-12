59d89fe5f0bd2d1952def244dc8a9e01f0ed8734
Ataberk Çiftlikli <64265169+ataberkciftlikli@users.noreply.github.com>
2025-04-12 15:32:59 +0300

    revert

--- backend/bagandgo/api/admin.py
+++ backend/bagandgo/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import AuthToken, UserProfile, ProductCategory, Product, Bag, Order
+from .models import AuthToken, UserProfile, ProductCategory, Product, Bag, Order, LikedProduct
 
 admin.site.register(AuthToken)
 admin.site.register(UserProfile)
@@ -7,3 +7,4 @@ admin.site.register(ProductCategory)
 admin.site.register(Product)
 admin.site.register(Bag)
 admin.site.register(Order)
+admin.site.register(LikedProduct)
