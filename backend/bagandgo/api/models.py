5005931987766e600801e0f4d64219fb0c3d2260
Ataberk Çiftlikli <64265169+ataberkciftlikli@users.noreply.github.com>
2025-04-12 15:31:38 +0300

    revert

--- backend/bagandgo/api/models.py
+++ backend/bagandgo/api/models.py
@@ -16,9 +16,11 @@ class AuthToken(models.Model):
 
 class UserProfile(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    address = models.CharField(max_length=255)
+    tc = models.CharField(max_length=11, null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    birth_year = models.IntegerField(null=True, blank=True)
     balance = models.FloatField(default=0)
-
+    
     def __str__(self):
         return self.user.username
 
@@ -32,6 +34,8 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     price = models.FloatField()
+    old_price = models.FloatField(default=0)
+    is_discounted = models.BooleanField(default=False)
     image = models.ImageField(upload_to='product_images/')
     stock = models.IntegerField()
     barcode = models.CharField(max_length=255)
@@ -43,7 +47,8 @@ class Product(models.Model):
 
 class Bag(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -59,3 +64,11 @@ class Order(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.confirmation_code}"
+
+class LikedProduct(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.product.name}"
